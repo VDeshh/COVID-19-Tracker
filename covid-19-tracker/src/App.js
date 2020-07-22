0abb9cecfcc4e6306afbaa0dedb6e1ebf3e64144
Vishal Desh <emailtovishy@gmail.com>
2020-07-21 17:53:00 -0700

    dropdown update

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "India"]);
+
+  //https://disease.sh/v3/covid-19/countries
+
   return (
     <div className="App">
-      <h1>Let's Go!</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop thru all countries and shop a drop down */}
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option 4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+      {/* Title + Select dropdown */}
+
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
